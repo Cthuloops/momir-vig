26a4a94d377a466727b7d7bb7e3d52895251250d
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2026-06-09 11:30:27 -0400

    feat(src/scryfall): start curl things.

---
+++ src/scryfall.c
@@ -0,0 +1,116 @@
+#include <errno.h>
+#include <stdio.h>
+#include <string.h>
+
+#include "scryfall.h"
+
+#define BULK_DATA_URL "https://api.scryfall.com/bulk-data"
+#define USER_AGENT "User-Agent: MomirVigApp/0.1"
+#define ACCEPT "Accept: */*"
+
+
+static size_t write_cb(void *contents, size_t size, size_t memb, void *userp) {
+    size_t realsize = size * memb;
+
+    struct CurlFatStruct *mem = (struct CurlFatStruct *)userp;
+
+    char *ptr = realloc(mem->memory, mem->size + realsize + 1);
+    if (!ptr) {
+        fprintf(stderr, "Error %i from write_cb: %s\n", errno, strerror(errno));
+        return 0;
+    }
+
+    mem->memory = ptr;
+    memcpy(&(mem->memory[mem->size]), contents, realsize);
+    mem->size += realsize;
+    mem->memory[mem->size] = 0;
+
+    return realsize;
+}
+
+
+int curl_init(struct CurlFatStruct *cfs) {
+    int out_status = 1;
+    struct curl_slist *list = NULL;
+
+    if (!cfs) {
+        fprintf(stderr, "Passed NULL to curl_init\n");
+        goto out;
+    }
+
+    CURLcode result = curl_global_init(CURL_GLOBAL_ALL);
+    if (result) {
+        fprintf(stderr, "Failed to initialize curl: %s\n",
+                curl_easy_strerror(result));
+        out_status = (int)result;
+        goto out;
+    }
+
+    cfs->memory = malloc(1);
+    if (!cfs->memory) {
+        fprintf(stderr, "Error %i from malloc: %s\n", errno, strerror(errno));
+        out_status = errno;
+        goto out;
+    }
+    cfs->size = 1;
+
+    cfs->curl = curl_easy_init();
+    if (!cfs->curl) {
+        fprintf(stderr, "Something went wrong with curl_easy_init()\n");
+        goto cleanup_memory;
+    }
+
+    // Write the information from requests into the struct.
+    curl_easy_setopt(cfs->curl, CURLOPT_WRITEFUNCTION, write_cb);
+    curl_easy_setopt(cfs->curl, CURLOPT_WRITEDATA, (void *)cfs);
+    
+    // Attach the required headers.
+    list = curl_slist_append(list, USER_AGENT);
+    list = curl_slist_append(list, ACCEPT);
+    curl_easy_setopt(cfs->curl, CURLOPT_HTTPHEADER, list);
+
+    goto out;
+
+cleanup_memory:
+    free(cfs->memory);
+
+out:
+    if (list) {
+        curl_slist_free_all(list);
+    }
+    return out_status;
+}
+
+
+int scryfall_bulk_data(struct CurlFatStruct *cfs) {
+    int out_status = -1;
+    if (!cfs) {
+        fprintf(stderr, "Passed NULL to scryfall_bulk_data()\n");
+        goto out;
+    }
+
+    if (!cfs->curl) {
+        fprintf(stderr, "Curl is not intialized in scryfall_bulk_data()\n");
+        goto out;
+    }
+
+    if (!cfs->memory) {
+        fprintf(stderr, "Memory is not initialized in scryfall_bulk_data()\n");
+        goto out;
+    }
+
+    curl_easy_setopt(cfs->curl, CURLOPT_URL, BULK_DATA_URL);
+
+    CURLcode result = curl_easy_perform(cfs->curl);
+    if (result != CURLE_OK) {
+        fprintf(stderr, "curl_easy_perform() failed: %s\n",
+                curl_easy_strerror(result));
+        out_status = (int)result;
+        goto out;
+    }
+
+    out_status = 1;
+
+out:
+    return out_status;
+}
