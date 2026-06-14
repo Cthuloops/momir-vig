404d9770559b9e7d71271e20f4d0fe782e400eea
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2026-06-14 15:48:15 -0400

    feat: add constants

---
+++ src/constants.h
@@ -0,0 +1,7 @@
+#ifndef MOMIR_CONSTANTS_H_
+#define MOMIR_CONSTANTS_H_
+
+#define MOMIR_DATA_DIR "./data/"
+#define MOMIR_BULK_DATA_PATH MOMIR_DATA_DIR "bulk_data.json"
+
+#endif // MOMIR_CONSTANTS_H_
