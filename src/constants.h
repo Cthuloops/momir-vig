ceacbcd18ba197ed202de4aeec4abbe8ca69c267
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2026-06-14 16:04:12 -0400

    chore(constants): update constant definitions

--- src/constants.h
+++ src/constants.h
@@ -4,4 +4,13 @@
 #define MOMIR_DATA_DIR "./data/"
 #define MOMIR_BULK_DATA_PATH MOMIR_DATA_DIR "bulk_data.json"
 
+// HTTP_CLIENT
+#define SCRYFALL_BULK_DATA_URL "https://api.scryfall.com/bulk-data"
+#define SCRYFALL_USER_AGENT "User-Agent: MomirVigApp/0.1"
+#define SCRYFALL_ACCEPT "Accept: */*"
+
+// MODULES
+#define HTTP_CLIENT "HTTP_CLIENT"
+#define PRINTER "PRINTER"
+
 #endif // MOMIR_CONSTANTS_H_
