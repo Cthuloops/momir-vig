295a786a1f4e49de190bdad347eb1f7e2da023bf
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2026-06-30 16:24:49 -0400

    feat(utils): add utils

--- src/utils.c
+++ src/utils.c
@@ -1,8 +1,14 @@
+#define _XOPEN_SOURCE
+
 #include <errno.h>
 #include <fcntl.h>
+#include <stdio.h>
 #include <string.h>
 #include <sys/stat.h>
+#include <time.h>
 #include <unistd.h>
+
+#include "../vendor/cJSON.h"
 
 #include "constants.h"
 #include "logging.h"
@@ -98,4 +104,131 @@ file_exists (const char *file_path, bool is_dir, bool create)
     }
 
   return false;
+}
+
+bool
+bulk_file_needs_update (const char *file_path)
+{
+  if (!file_path || strcmp (file_path, "") == 0)
+    {
+      LOG_ERROR (MOMIR_UTILS,
+                 "Passed NULL or empty string to bulk_file_needs_update");
+      return false;
+    }
+
+  if (!file_exists (file_path, false, false))
+    {
+      LOG_ERROR (MOMIR_UTILS, "File %s does not exist");
+      return false;
+    }
+
+  struct stat stats;
+  if (stat (file_path, &stats) == 0)
+    {
+      if (!S_ISREG (stats.st_mode))
+        {
+          LOG_ERROR (MOMIR_UTILS, "Path type mismatch for %s (Expected file)",
+                     file_path);
+          return false;
+        }
+    }
+  else
+    {
+      LOG_ERROR (MOMIR_UTILS, "Error from stat at %s: %s", file_path,
+                 strerror (errno));
+      return false;
+    }
+
+  bool out_status = false;
+
+  const size_t bulk_string_size = stats.st_size + 1;
+  char *bulk_string = malloc (bulk_string_size);
+  if (!bulk_string)
+    {
+      LOG_ERROR (MOMIR_UTILS, "Failed to allocate %zu bytes for %s file",
+                 bulk_string_size, file_path);
+      goto cleanup_bulk_string;
+    }
+
+  FILE *fp = fopen (file_path, "r");
+  if (!fp)
+    {
+      LOG_ERROR (MOMIR_UTILS, "Failed to open file %s", file_path);
+      goto cleanup_bulk_file;
+    }
+
+  fgets (bulk_string, bulk_string_size, fp);
+  bulk_string[stats.st_size] = '\0';
+
+  cJSON *bulk_json = cJSON_Parse (bulk_string);
+  if (!bulk_json)
+    {
+      const char *err = cJSON_GetErrorPtr ();
+      if (err)
+        {
+          LOG_ERROR (MOMIR_UTILS, "Error cJSON_Parse: %s", err);
+          goto cleanup_cjson;
+        }
+    }
+
+  const cJSON *data = cJSON_GetObjectItem (bulk_json, "data");
+  if (!data)
+    {
+      LOG_ERROR (MOMIR_UTILS, "Item 'data' not found in %s", file_path);
+      goto cleanup_cjson;
+    }
+
+  // Search for the default card object and check the last time it was updated.
+  struct tm last_updated_calender = { 0 };
+  const cJSON *datum;
+  cJSON_ArrayForEach (datum, data)
+  {
+    const cJSON *type = cJSON_GetObjectItem (datum, "type");
+    if (!type)
+      {
+        LOG_ERROR (MOMIR_UTILS, "Item 'type' not found in data");
+        goto cleanup_cjson;
+      }
+    if (cJSON_IsString (type) && strcmp (type->valuestring, "default_cards"))
+      {
+        const cJSON *updated_at = cJSON_GetObjectItem (datum, "updated_at");
+        if (!updated_at)
+          {
+            LOG_ERROR (MOMIR_UTILS, "Item 'updated_at' not found");
+            goto cleanup_cjson;
+          }
+
+        if (cJSON_IsString (updated_at))
+          {
+            // ignore the return, we only yyyy-mm-dd.
+            strptime (updated_at->valuestring, "%Y-%m-%d",
+                      &last_updated_calender);
+            if (difftime (time (NULL), mktime (&last_updated_calender))
+                >= SCRYFALL_BULK_DATA_UPDATE_TIME)
+              {
+                out_status = true;
+                goto cleanup_cjson;
+              }
+
+            goto cleanup_cjson;
+          }
+      }
+  }
+
+cleanup_cjson:
+  cJSON_Delete (bulk_json);
+
+cleanup_bulk_file:
+  if (fp)
+    {
+      // fclose (fp);
+    }
+
+cleanup_bulk_string:
+  if (bulk_string)
+    {
+      // free (bulk_string);
+    }
+
+  return out_status;
 }
