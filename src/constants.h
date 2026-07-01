1aaae4d1dd7fb434fc35cfea1ff29f8ad804b423
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2026-06-30 21:45:45 -0400

    chore(constants): update scryfall constant scheme

--- src/constants.h
+++ src/constants.h
@@ -5,10 +5,15 @@
 #define MOMIR_BULK_DATA_PATH MOMIR_DATA_DIR "bulk_data.json"
 
 // HTTP_CLIENT
-#define SCRYFALL_BULK_DATA_URL "https://api.scryfall.com/bulk-data"
-#define SCRYFALL_USER_AGENT "User-Agent: MomirVigApp/0.1"
+// Scryfall API
+#define SCRYFALL_API_URL "https://api.scryfall.com"
+#define SCRYFALL_BULK_URL SCRYFALL_API_URL "/bulk-data"
+#define SCRYFALL_DEFAULT_CARDS_URL SCRYFALL_BULK_URL "/default-cards"
+
+#define SCRYFALL_HEADER_USER_AGENT "User-Agent: MomirVigApp/0.1"
 #define SCRYFALL_ACCEPT "Accept: */*"
-#define SCRYFALL_BULK_DATA_UPDATE_TIME 604800 // Amount of seconds in 7 days.
+
+#define SCRYFALL_BULK_DATA_UPDATE_TIME 604800L // Amount of seconds in 7 days.
 
 // MODULES
 #define MOMIR_MAIN "MOMIR_MAIN"
--- src/http_client.h
+++ src/http_client.h
@@ -2,6 +2,7 @@
 #define HTTP_CLIENT_H_
 
 #include <stdlib.h>
+#include <time.h>
 
 #include <curl/curl.h>
 
@@ -11,6 +12,37 @@ struct CurlFatStruct
   char *memory;
   struct curl_slist *list;
   size_t size;
+};
+
+// {
+//   "object": "bulk_data",
+//   "id": "e2ef41e3-5778-4bc2-af3f-78eca4dd9c23",
+//   "type": "default_cards",
+//   "updated_at": "2026-06-30T21:09:15.220+00:00",
+//   "uri":
+//   "https://api.scryfall.com/bulk-data/e2ef41e3-5778-4bc2-af3f-78eca4dd9c23",
+//   "name": "Default Cards",
+//   "description": "A JSON file containing every card object on Scryfall in
+//   English or the printed language if the card is only available in one
+//   language.", "size": 555909537, "download_uri":
+//   "https://data.scryfall.io/default-cards/default-cards-20260630210915.json",
+//   "content_type": "application/json",
+//   "content_encoding": "gzip"
+// },
+
+/**
+ * Represents a bulk data item from Scryfall API.
+ */
+struct BulkDataItem
+{
+  enum
+  {
+    SCRYFALL_BULK_DEFAULT_CARDS,
+  } type;
+  time_t updated_at;
+  size_t download_size;
+  char *uri;
+  char *download_uri;
 };
 
 /**
