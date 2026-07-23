c4912383953126a0868fcddda250c2deda99f332
Harley Coughlin <104121493+Cthuloops@users.noreply.github.com>
2026-07-23 15:05:03 -0400

    feat(utils): flatten call structure

--- src/utils.h
+++ src/utils.h
@@ -7,8 +7,6 @@
 
 #include <stdbool.h>
 
-#include "http_client.h"
-
 /**
  * @brief file_exists checks if a file at the given path exists
  *
@@ -27,30 +25,5 @@ bool file_exists (const char *file_path, bool is_dir);
  * @return True if the directory exists/created, false otherwise.
  */
 bool create_dir (const char *dir_path);
-
-/**
- * @brief bulk_file_needs_update checks if the bulk file needs to be updated.
- *
- * @param[in] file_path Path to the bulk file.
- *
- * @return True if SCRYFALL_UPDATE_TIME has passed since the file was last,
- * updated. False otherwise, or if there was an error.
- */
-bool bulk_file_needs_update (const char *file_path);
-
-/**
- * @brief bulk_data_item_up_to_date checks if the data item is considered up to
- * date.
- *
- * Checks to see if a data item has been updated within
- * #SCRYFALL_BULK_DATA_UPDATE_TIME
- *
- * @param[in] bdi Pointer to a BulkDataItem struct.
- *
- * @return true if the data is considered up to date, false otherwise.
- *
- * @see struct BulkDataItem
- */
-bool bulk_data_item_up_to_date (const struct BulkDataItem *bdi);
 
 #endif // MOMIR_UTILS_H_
